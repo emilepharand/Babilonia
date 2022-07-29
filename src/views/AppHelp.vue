e6e2cc799d51d01428ee43e8036e5cef9e9a0660
Émile Pharand <emile.pharand@gmail.com>
2022-07-29 17:29:23 -0400

    Remove help page component definition

--- src/views/AppHelp.vue
+++ src/views/AppHelp.vue
@@ -3,15 +3,3 @@
     <h1>Help</h1>
   </div>
 </template>
-
-<script lang="ts">
-import {defineComponent} from 'vue';
-
-export default defineComponent({
-	name: 'AppHelp',
-	async created() {
-	},
-	methods: {
-	},
-});
-</script>
