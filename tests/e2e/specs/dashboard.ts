8cfe59fd4ddc82bb7de55a13fe753ca40ae57d38
Émile Pharand <emile.pharand@gmail.com>
2022-07-28 20:32:21 -0400

    Use more diverse set for ideas in dashboard test

--- tests/e2e/specs/dashboard.ts
+++ tests/e2e/specs/dashboard.ts
@@ -8,27 +8,30 @@ before(() => {
 
 context('Practicing', () => {
 	specify('Practicing works', () => {
-		cyutils.addIdeas();
+		cyutils.addIdeasDifferentSet();
 
 		cy.reload();
 
 		cy.get('.dashboard-row')
-			.should('have.length', 5);
+			.should('have.length', 6);
 
 		cy.get('.dashboard-row').eq(0)
 			.should('contain', 'français')
-			.should('contain', '2');
+			.should('contain', '3');
 		cy.get('.dashboard-row').eq(1)
 			.should('contain', 'english')
-			.should('contain', '2');
+			.should('contain', '3');
 		cy.get('.dashboard-row').eq(2)
 			.should('contain', 'español')
-			.should('contain', '2');
+			.should('contain', '3');
 		cy.get('.dashboard-row').eq(3)
 			.should('contain', 'italiano')
-			.should('contain', '2');
+			.should('contain', '1');
 		cy.get('.dashboard-row').eq(4)
 			.should('contain', 'deutsch')
+			.should('contain', '2');
+		cy.get('.dashboard-row').eq(5)
+			.should('contain', 'português')
 			.should('contain', '2');
 	});
 });
