af4768b418170778f919376f860b69206cc730e5
Émile Pharand <emile.pharand@gmail.com>
2022-08-04 19:45:46 -0400

    Convert IdeaForm to Composition API

--- src/components/IdeaForm.vue
+++ src/components/IdeaForm.vue
@@ -13,30 +13,23 @@
   </div>
 </template>
 
-<script lang="ts">
-import {defineComponent} from 'vue';
-import Api from '@/ts/api';
+<script lang="ts" setup>
+import {ref} from 'vue';
 import {getEmptyLanguagesNoAsync} from '../../server/model/languages/language';
+import Api from '@/ts/api';
 
-export default defineComponent({
-	name: 'IdeaForm',
-	props: {
-		title: String,
-		// TODO: This needs to be strictly Idea but right now it doesn't work
-		// Maybe when we use the Composition API which has first-class TS support
-		idea: Object,
-	},
-	data() {
-		return {
-			languages: getEmptyLanguagesNoAsync(),
-			noLanguages: false,
-			loaded: false,
-		};
-	},
-	async created() {
-		this.languages = await Api.getLanguages();
-		this.loaded = true;
-	},
-	emits: ['addRows'],
+defineProps({
+	title: String,
+	idea: Object,
 });
+
+defineEmits(['addRows', 'delete']);
+
+const languages = ref(getEmptyLanguagesNoAsync());
+const loaded = ref(false);
+
+(async () => {
+	languages.value = await Api.getLanguages();
+	loaded.value = true;
+})();
 </script>
