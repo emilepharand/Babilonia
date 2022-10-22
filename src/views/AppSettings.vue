f39414d6a5f0929aeef9b47d7a319f291b6205bf
Émile Pharand <emile.pharand@gmail.com>
2022-10-22 17:55:10 -0400

    Fix use import instead of require

--- src/views/AppSettings.vue
+++ src/views/AppSettings.vue
@@ -65,6 +65,7 @@
 import {nextTick, ref} from 'vue';
 import Api from '../ts/api';
 import {getEmptySettingsNoAsync} from '../../server/model/settings/settings';
+import * as bootstrap from 'bootstrap';
 
 const settings = ref(getEmptySettingsNoAsync());
 const showSettingsSavedMessage = ref(false);
@@ -78,8 +79,6 @@ async function save() {
 	showSettingsSavedMessage.value = true;
 }
 
-// Tooltip popovers
-const bootstrap = require('bootstrap');
 nextTick(() => {
 	const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 	tooltipTriggerList.map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
