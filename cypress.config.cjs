bbf0c794860e07c41c63fbfaaa88e5edd118c44f
Émile Pharand <emile.pharand@gmail.com>
2022-10-24 18:34:43 -0400

    Add code coverage to Cypress

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,21 +1,17 @@
-import {defineConfig} from 'cypress';
+const { defineConfig } = require('cypress')
+require('dotenv').config({path: '.env.test'});
 
-import * as dotenv from 'dotenv';
-
-dotenv.config({path: '.env.test'});
-
-export default defineConfig({
+module.exports = defineConfig({
 	screenshotOnRunFailure: true,
 	video: false,
 	fixturesFolder: 'tests/e2e/fixtures',
 	screenshotsFolder: 'tests/e2e/screenshots',
 	videosFolder: 'tests/e2e/videos',
 	e2e: {
-		// // We've imported your old cypress plugins here.
-		// // You may want to clean this up later by importing these.
-		// setupNodeEvents(on, config) {
-		// 	return require('./tests/e2e/plugins/index.cjs')(on, config);
-		// },
+		setupNodeEvents(on, config) {
+			require('@cypress/code-coverage/task')(on, config)
+			return config
+		},
 		baseUrl: `${process.env.VITE_BASE_URL}`,
 		specPattern: 'tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}',
 		supportFile: 'tests/e2e/support/index.js',
