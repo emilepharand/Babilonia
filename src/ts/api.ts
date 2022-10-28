dc8998f755d069b9fc5d50b70489d143fafb360a
Émile Pharand <emile.pharand@gmail.com>
2022-10-27 21:47:34 -0400

    Remove dead code

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -77,9 +77,6 @@ export default class Api {
 			},
 			body: JSON.stringify({name}),
 		});
-		if (response.status !== 201) {
-			return Promise.reject();
-		}
 		return (await response.json()) as Language;
 	}
 
@@ -94,9 +91,6 @@ export default class Api {
 			},
 			body: JSON.stringify(languages),
 		});
-		if (response.status === 400) {
-			return Promise.reject();
-		}
 		return (await response.json()) as Language[];
 	}
 
