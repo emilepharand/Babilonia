98a6ff8dac9c3d718c0d084ed857af52570c17af
Émile Pharand <emile.pharand@gmail.com>
2022-11-06 19:32:54 -0500

    Add tests for context handling when practicing

--- tests/e2e/specs/practicing.cy.ts
+++ tests/e2e/specs/practicing.cy.ts
@@ -218,11 +218,12 @@ context.only('Context', () => {
 		addLanguages();
 		const e1: ExpressionForAdding = {languageId: 1, text: '(bien le) bonjour (à vous)'};
 		const e2: ExpressionForAdding = {languageId: 1, text: 'salut (mon) cher'};
-		const e3: ExpressionForAdding = {languageId: 2, text: 'hello'};
-		const e4: ExpressionForAdding = {languageId: 3, text: 'buenos días'};
-		const e5: ExpressionForAdding = {languageId: 4, text: 'buongiorno'};
-		const e6: ExpressionForAdding = {languageId: 5, text: 'guten Tag'};
-		const i1: IdeaForAdding = {ee: [e1, e2, e3, e4, e5, e6]};
+		const e3: ExpressionForAdding = {languageId: 1, text: 'salut (mon) (bel) ami'};
+		const e4: ExpressionForAdding = {languageId: 2, text: 'hello'};
+		const e5: ExpressionForAdding = {languageId: 3, text: 'buenos días'};
+		const e6: ExpressionForAdding = {languageId: 4, text: 'buongiorno'};
+		const e7: ExpressionForAdding = {languageId: 5, text: 'guten Tag'};
+		const i1: IdeaForAdding = {ee: [e1, e2, e3, e4, e5, e6, e7]};
 		cy.request({
 			url: `${apiUrl}/ideas`,
 			method: 'POST',
@@ -247,37 +248,72 @@ context.only('Context', () => {
 
 		cy.get('#practice-link').click();
 
-		waitForTableToLoad(5);
+		waitForTableToLoad(7);
 
 		// Context at beginning of expression is shown
-		assertIsTyped(0, '(bien le)');
-		assertRowMatchIsNeutral(0);
+		assertIsTyped(1, '(bien le) ');
+		assertRowMatchIsPartialMatch(1);
 
 		// Start to type
-		typeInRow(0, 'b');
-		assertIsTyped(0, '(bien le) b');
+		typeInRow(1, 'b');
+		assertIsTyped(1, '(bien le) b');
 
 		// Complete
-		assertRowMatchIsPartialMatch(0);
-		typeInRow(0, 'bonjour');
-		assertRowMatchIsFullMatch(0, '(bien le) bonjour (à vous)');
+		assertRowMatchIsPartialMatch(1);
+		typeInRow(1, 'onjour');
+		assertRowMatchIsFullMatch(1, '(bien le) bonjour (à vous)');
 
 		// Context appears
-		typeInRow(1, 'salu');
-		assertIsTyped(1, 'salu');
-		typeInRow(1, 't');
-		assertIsTyped(1, 'salut (mon)');
-		assertRowMatchIsPartialMatch(1);
+		typeInRow(2, 'salu');
+		assertIsTyped(2, 'salu');
+		typeInRow(2, 't');
+		assertIsTyped(2, 'salut (mon) ');
+		assertRowMatchIsPartialMatch(2);
 
 		// Don't have to type space
-		typeInRow(1, 'c');
-		assertIsTyped(1, 'salut (mon) c');
-		assertRowMatchIsPartialMatch(1);
+		typeInRow(2, 'c');
+		assertIsTyped(2, 'salut (mon) c');
+		assertRowMatchIsPartialMatch(2);
 
 		// Complete
+		assertRowMatchIsPartialMatch(2);
+		typeInRow(2, 'her');
+		assertRowMatchIsFullMatch(2, 'salut (mon) cher');
+
+		// Two contexts in a row
+		typeInRow(3, 'salut');
+		assertIsTyped(3, 'salut (mon) (bel) ');
+		assertRowMatchIsPartialMatch(3);
+		typeInRow(3, 'ami');
+		assertRowMatchIsFullMatch(3, 'salut (mon) (bel) ami');
+
+		getResetButton().click();
+
+		// Hinting
+		getRowHintButton(1).click();
+		assertIsTyped(1, '(bien le) b');
 		assertRowMatchIsPartialMatch(1);
-		typeInRow(1, 'her');
-		assertRowMatchIsFullMatch(1, 'salut (mon) cher');
+		typeInRow(1, 'onjou');
+		assertIsTyped(1, '(bien le) bonjou');
+		assertRowMatchIsPartialMatch(1);
+		getRowHintButton(1).click();
+		assertRowMatchIsFullMatch(1, '(bien le) bonjour (à vous)');
+
+		typeInRow(2, 'salu');
+		getRowHintButton(2).click();
+		assertIsTyped(2, 'salut (mon) ');
+		getRowHintButton(2).click();
+		assertIsTyped(2, 'salut (mon) c');
+		getRowShowButton(2).click();
+		assertRowMatchIsFullMatch(2, 'salut (mon) cher');
+
+		typeInRow(3, 'salu');
+		getRowHintButton(3).click();
+		assertIsTyped(3, 'salut (mon) (bel) ');
+		getRowHintButton(3).click();
+		assertIsTyped(3, 'salut (mon) (bel) a');
+		getRowShowButton(3).click();
+		assertRowMatchIsFullMatch(3, 'salut (mon) (bel) ami');
 	});
 });
 
