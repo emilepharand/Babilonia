7c3c1a0ef0d37a82183578d3f368893874922790
Émile Pharand <emile.pharand@gmail.com>
2022-11-12 17:13:30 -0500

    Add test for practice only not known expressions

--- tests/api/practicing.ts
+++ tests/api/practicing.ts
@@ -87,6 +87,7 @@ describe('getting practice ideas', () => {
 		let settings: Settings = {
 			randomPractice: true,
 			strictCharacters: false,
+			practiceOnlyNotKnown: false,
 		};
 		await setSettings(settings);
 
@@ -125,7 +126,7 @@ describe('getting practice ideas', () => {
 		// This test might pass when it should fail but the chance is statistically small
 		expect(idsInSameOrder).toEqual(false);
 
-		settings = {randomPractice: false, strictCharacters: false};
+		settings = {randomPractice: false, strictCharacters: false, practiceOnlyNotKnown: false};
 		await setSettings(settings);
 
 		firstIdeaIds = [];
@@ -203,6 +204,32 @@ describe('getting practice ideas', () => {
 			{languageId: noPracticeLanguage.id, text: 'expression2'}]};
 		await addIdea(ideaWithOnlyPracticeLanguage);
 		const fetchedIdeaWithPracticeAndNoPracticeLanguage = await addIdea(ideaWithPracticeAndNoPracticeLanguage);
+		expect((await nextPracticeIdea()).id).toEqual(fetchedIdeaWithPracticeAndNoPracticeLanguage.id);
+		expect((await nextPracticeIdea()).id).toEqual(fetchedIdeaWithPracticeAndNoPracticeLanguage.id);
+	});
+
+	test('ideas with only known expressions in practice languages are not practiceable depending on settings', async () => {
+		const practiceLanguage: Language = await addLanguage('practice language 1');
+		const practiceLanguage2: Language = await addLanguage('practice language 2');
+		const noPracticeLanguage: Language = await addLanguage('no practice language 1');
+		practiceLanguage.isPractice = true;
+		practiceLanguage2.isPractice = true;
+		await editLanguages([practiceLanguage, practiceLanguage2, noPracticeLanguage]);
+		const ideaWithOnlyKnownExpressions: IdeaForAdding = {ee: [{languageId: practiceLanguage.id, text: 'expression', known: true},
+			{languageId: noPracticeLanguage.id, text: 'expression2'}]};
+		const ideaWithPracticeAndNoPracticeLanguage: IdeaForAdding = {ee: [{languageId: practiceLanguage.id, text: 'expression'},
+			{languageId: noPracticeLanguage.id, text: 'expression2'}]};
+		const fetchedIdeaWithOnlyKnownExpressions = await addIdea(ideaWithOnlyKnownExpressions);
+		const fetchedIdeaWithPracticeAndNoPracticeLanguage = await addIdea(ideaWithPracticeAndNoPracticeLanguage);
+
+		// Practice only known is false
+		expect((await nextPracticeIdea()).id).toEqual(fetchedIdeaWithOnlyKnownExpressions.id);
+		expect((await nextPracticeIdea()).id).toEqual(fetchedIdeaWithPracticeAndNoPracticeLanguage.id);
+
+		// Practice only known is true
+		await setSettings({
+			practiceOnlyNotKnown: true, randomPractice: false, strictCharacters: false,
+		});
 		expect((await nextPracticeIdea()).id).toEqual(fetchedIdeaWithPracticeAndNoPracticeLanguage.id);
 		expect((await nextPracticeIdea()).id).toEqual(fetchedIdeaWithPracticeAndNoPracticeLanguage.id);
 	});
