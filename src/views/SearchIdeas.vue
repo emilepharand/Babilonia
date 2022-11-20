362c84d89e9503ad13ab5a3accff849139e31801
Émile Pharand <emile.pharand@gmail.com>
2022-11-19 21:57:18 -0500

    Add paging on the search page

--- src/views/SearchIdeas.vue
+++ src/views/SearchIdeas.vue
@@ -165,7 +165,7 @@
         class="ps-3 d-flex flex-column"
         style="width:500px"
       >
-        <h2 v-if="noResults">
+        <h2 v-if="noResults()">
           No results.
         </h2>
         <div
@@ -192,6 +192,17 @@
             </div>
           </a>
         </div>
+        <div
+          v-if="!noResults()"
+          class="me-3 mt-3"
+        >
+          <button
+            class="btn btn-primary w-100"
+            @click="nextPage()"
+          >
+            Next page
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -199,11 +210,12 @@
 
 <script lang="ts" setup>
 import {ref} from 'vue';
-import * as Api from '../ts/api';
+import type {Idea} from '../../server/model/ideas/idea';
+import {getEmptyIdeaArrayNoAsync} from '../../server/model/ideas/idea';
 import type {Language} from '../../server/model/languages/language';
 import {getEmptyLanguageNoAsync, getEmptyLanguagesNoAsync} from '../../server/model/languages/language';
 import type {SearchContext} from '../../server/model/search/searchContext';
-import {getEmptyIdeaArrayNoAsync} from '../../server/model/ideas/idea';
+import * as Api from '../ts/api';
 
 // Search context
 const pattern = ref('');
@@ -214,13 +226,15 @@ const expressionLanguage = ref(getEmptyLanguageNoAsync());
 const ideaDoesNotHave = ref(getEmptyLanguageNoAsync());
 const knownExpressions = ref(false);
 const unknownExpressions = ref(false);
+let currentPage = 0;
+const pageSize = 10;
+let searchResults: Idea[];
 
 // Component data
 const languagesWithPlaceholder = ref(getEmptyLanguagesNoAsync());
 const languages = ref(getEmptyLanguagesNoAsync());
 const isShowError = ref(false);
 const errorText = ref('');
-const noResults = ref(false);
 const languagePlaceholderId = -1;
 
 (async () => {
@@ -253,10 +267,10 @@ async function search() {
 		isShowError.value = true;
 		return;
 	}
+	currentPage = 0;
 	const searchContext = createSearchContext();
-	const matchedIdeas = await Api.searchIdeas(searchContext);
-	noResults.value = matchedIdeas.length === 0;
-	results.value = matchedIdeas;
+	searchResults = await Api.searchIdeas(searchContext);
+	results.value = searchResults.slice(0, pageSize);
 }
 
 function atLeastOneFilterSet() {
@@ -266,22 +280,30 @@ function atLeastOneFilterSet() {
 }
 
 function createSearchContext() {
-	const sc2: SearchContext = {};
-	sc2.strict = strict.value;
-	sc2.pattern = pattern.value;
+	const sc: SearchContext = {};
+	sc.strict = strict.value;
+	sc.pattern = pattern.value;
 	if (ideaDoesNotHave.value.id !== languagePlaceholderId) {
-		sc2.ideaDoesNotHave = ideaDoesNotHave.value.id;
+		sc.ideaDoesNotHave = ideaDoesNotHave.value.id;
 	}
 	if (ideaHas.value.length > 0) {
-		sc2.ideaHas = ideaHas.value.map((l: Language) => l.id);
+		sc.ideaHas = ideaHas.value.map((l: Language) => l.id);
 	}
 	if (expressionLanguage.value.id !== languagePlaceholderId) {
-		sc2.language = expressionLanguage.value.id;
+		sc.language = expressionLanguage.value.id;
 	}
 	if (knownExpressions.value || unknownExpressions.value) {
-		sc2.knownExpressions = knownExpressions.value;
+		sc.knownExpressions = knownExpressions.value;
 	}
-	return sc2;
+	return sc;
 }
 
+async function nextPage() {
+	currentPage++;
+	results.value = searchResults.slice(currentPage * pageSize, currentPage + pageSize);
+}
+
+function noResults() {
+	return searchResults.slice(currentPage * pageSize, currentPage + pageSize).length === 0;
+}
 </script>
