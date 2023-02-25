f308d827956325f02c5bec49fd5863c8c2c36dd4
Émile Pharand <emile.pharand@gmail.com>
2023-02-25 17:05:01 -0500

    Improve Bash test script

--- tests/bash/tests.sh
+++ tests/bash/tests.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+write_coverage() {
+  local output_file="$1"
+  curl -sf "localhost:$VITE_BASE_PORT/__coverage__" | cut -c13- | sed 's/.$//' > "../tests/coverage/merged/${output_file}"
+}
+
 dbFileName="newDbFile.db"
 
 cd dist
 
 ####################################################
-# --db=db (file does not exist) file is created
+# Test 1
 ####################################################
 
-echo "----------------------------------"
-echo "Test 1"
-echo "----------------------------------"
+echo "------------------------------------------------------"
+echo " Test 1                                               "
+echo "------------------------------------------------------"
+echo " --db=db, file does not exist, file is created        "
+echo "------------------------------------------------------"
 
-# Sanity check
+# Previous test may have left a file
 if [ -f "$dbFileName" ]; then
-    echo "File $dbFileName already exists."
+    echo "File $dbFileName already exists. Deleting."
     rm $dbFileName
-    # exit 1
 fi
 
 # Run
@@ -25,10 +31,10 @@ PID=$!
 sleep 1
 
 # Coverage
-curl localhost:$VITE_BASE_PORT/__coverage__ | cut -c13- | rev | cut -c2- | rev > ../tests/coverage/merged/coverage-bash.json
+write_coverage "coverage-bash.json"
 
-# Create data for next test
-curl "localhost:$VITE_API_PORT/languages" -H "Content-Type: application/json" -d '{"name":"newLanguage"}'
+# Create data for the next test
+curl -sfq "localhost:$VITE_API_PORT/languages" -H "Content-Type: application/json" -d '{"name":"newLanguage"}' > /dev/null
 
 kill $PID
 
@@ -36,21 +42,27 @@ dbFileSize=$(stat -c%s "$dbFileName")
 
 # Test that database file has been created and is not empty
 if [ ! -f "$dbFileName" ]; then
+    echo "--> Result: failure!"
     echo "File $dbFileName was not created."
     exit 1
 elif [ ! -s "$dbFileName" ]
 then 
+   echo "--> Result: failure!"
    echo "File $dbFileName was created but is empty."
    exit 1
+else
+   echo -e "\n--> Result: success!\n"
 fi
 
 ####################################################
-# --db=db (file exists) file is not overwritten
+# Test 2
 ####################################################
 
-echo "----------------------------------"
-echo "Test 2"
-echo "----------------------------------"
+echo "------------------------------------------------------"
+echo " Test 2                                               "
+echo "------------------------------------------------------"
+echo " --db=db, file exists, file is not overwritten        "
+echo "------------------------------------------------------"
 
 # Run
 node index.cjs --db="$dbFileName" &
@@ -58,37 +70,45 @@ PID=$!
 sleep 1
 
 # Coverage
-curl localhost:$VITE_BASE_PORT/__coverage__ | cut -c13- | rev | cut -c2- | rev > ../tests/coverage/merged/coverage-bash-2.json
+write_coverage "coverage-bash-2.json"
 
-res=$(curl localhost:$VITE_API_PORT/languages/1 -H "Content-Type: application/json")
+res=$(curl -sf localhost:$VITE_API_PORT/languages/1 -H "Content-Type: application/json")
 
 if [ "$res" != '{"id":1,"isPractice":false,"name":"newLanguage","ordering":0}' ]; then
+    echo "Result: failure!"
     echo "Database was overwritten."
     kill $PID
     exit 1
+else
+    echo -e "\n--> Result: success!\n"
 fi
 
 kill $PID
 
 ######################################
-# --dev-mode does not start webserver
+# Test 3
 ######################################
 
-echo "----------------------------------"
-echo "Test 3"
-echo "----------------------------------"
+echo "------------------------------------------------------"
+echo " Test 3                                               "
+echo "------------------------------------------------------"
+echo " --dev-mode does not start webserver                  "
+echo "------------------------------------------------------"
 
 node index.cjs --dev-mode &
 PID=$!
 sleep 1
 
 # Coverage
-curl localhost:$VITE_API_PORT/__coverage__ | cut -c13- | rev | cut -c2- | rev > ../tests/coverage/merged/coverage-bash-3.json
+write_coverage "coverage-bash-3.json"
 
-if curl --output /dev/null --silent --head --fail "localhost:$VITE_BASE_PORT"; then
+if curl -sf --output /dev/null --silent --head --fail "localhost:$VITE_BASE_PORT"; then
+  echo "--> Result: failure!"
   echo "URL exists: localhost:$VITE_BASE_PORT"
   kill $PID
   exit 1
+else
+  echo -e "\n--> Result: success!\n"
 fi
 
 kill $PID
