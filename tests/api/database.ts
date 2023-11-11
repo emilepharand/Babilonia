3da44fd797edc66ab612524de427560da2d983e3
emilepharand <emile.pharand@gmail.com>
2023-11-10 20:24:46 -0500

    Fix test

--- tests/api/database.ts
+++ tests/api/database.ts
@@ -8,7 +8,7 @@ describe('2.0', () => {
 	test('change database', async () => {
 		let ll = await fetchLanguages();
 		expect(ll).toHaveLength(0);
-		await changeDatabase('tests/db/2.0.db');
+		await changeDatabase('../tests/db/2.0.db');
 		ll = await fetchLanguages();
 		expect(ll).toHaveLength(1);
 		expect(ll[0]).toEqual({id: 1, name: '2.0-l1', ordering: 0, isPractice: true});
