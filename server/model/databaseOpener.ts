52bbd05551c77fbedec89b5ec44b663ba79a89c4
emilepharand <emile.pharand@gmail.com>
2023-11-11 19:02:59 -0500

    Add log when opening database

--- server/model/databaseOpener.ts
+++ server/model/databaseOpener.ts
@@ -4,6 +4,9 @@ import sqlite3 from 'sqlite3';
 import type {Database} from 'sqlite';
 
 export async function initDatabase(path: string): Promise<Database> {
+	if (!fs.existsSync(path)) {
+		console.log(`Database ${path} does not exist, it will be created.`);
+	}
 	const db = await open({
 		filename: path,
 		driver: sqlite3.Database,
