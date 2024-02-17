a78500d6e285793134f8408cd062a205a789a01c
Émile Pharand <emile.pharand@gmail.com>
2024-02-16 22:24:30 -0500

    Formatting and consistency

--- server/model/databaseInitializer.ts
+++ server/model/databaseInitializer.ts
@@ -2,38 +2,38 @@ import {type Database} from 'sqlite';
 
 export async function clearDatabaseAndCreateSchema(db: Database) {
 	console.log('WARNING: The database is about to be cleared and reinitialized.');
-	await db.run('drop table if exists expressions');
-	await db.run('drop table if exists ideas');
-	await db.run('drop table if exists languages');
-	await db.run('drop table if exists settings');
-	await db.run(
-		'CREATE TABLE "languages" (\n'
-        + '\t"id"\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,\n'
-        + '\t"name"\tTEXT NOT NULL,\n'
-        + '\t"ordering"\tINTEGER NOT NULL,\n'
-        + '\t"isPractice"\tTEXT NOT NULL\n'
-        + ')',
-	);
-	await db.run(
-		'CREATE TABLE "ideas" (\n'
-        + '\t"id"\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE\n'
-        + ')',
-	);
-	await db.run(
-		'CREATE TABLE "expressions" (\n'
-        + '\t"id"\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,\n'
-        + '\t"ideaId"\tINTEGER NOT NULL,\n'
-        + '\t"languageId"\tINTEGER NOT NULL,\n'
-        + '\t"text"\tTEXT NOT NULL,\n'
-        + '\t"known"\tTEXT DEFAULT 0,\n'
-        + '\tFOREIGN KEY("languageId") REFERENCES "languages"("id"),\n'
-        + '\tFOREIGN KEY("ideaId") REFERENCES "ideas"("id")\n'
-        + ')',
-	);
-	await db.run(
-		'CREATE TABLE "settings" (\n'
-        + '"name"\tTEXT NOT NULL UNIQUE,\n'
-        + '"value"\tTEXT\n'
-        + ')',
-	);
+	await db.run('DROP TABLE IF EXISTS expressions');
+	await db.run('DROP TABLE IF EXISTS ideas');
+	await db.run('DROP TABLE IF EXISTS languages');
+	await db.run('DROP TABLE IF EXISTS settings');
+	await db.run(`
+        CREATE TABLE "languages" (
+            "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            "name" TEXT NOT NULL,
+            "ordering" INTEGER NOT NULL,
+            "isPractice" TEXT NOT NULL
+        )
+    `);
+	await db.run(`
+        CREATE TABLE "ideas" (
+            "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE
+        )
+    `);
+	await db.run(`
+        CREATE TABLE "expressions" (
+            "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            "ideaId" INTEGER NOT NULL,
+            "languageId" INTEGER NOT NULL,
+            "text" TEXT NOT NULL,
+            "known" TEXT DEFAULT 0,
+            FOREIGN KEY("languageId") REFERENCES "languages"("id"),
+            FOREIGN KEY("ideaId") REFERENCES "ideas"("id")
+        )
+    `);
+	await db.run(`
+        CREATE TABLE "settings" (
+            "name" TEXT NOT NULL UNIQUE,
+            "value" TEXT
+        )
+    `);
 }
