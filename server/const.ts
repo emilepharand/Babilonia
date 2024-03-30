006b9b30ec118edf4c9df598a118c61495ffe9df
Émile Pharand <emile.pharand@gmail.com>
2024-03-30 19:12:13 -0400

    Change current version constant to 2.2

--- server/const.ts
+++ server/const.ts
@@ -1,4 +1,4 @@
-export const currentVersion = '2.1';
+export const currentVersion = '2.2';
 export const memoryDatabasePath = ':memory:';
 export const databaseVersionErrorCode = 'DATABASE_VERSION';
 export const baseDatabasePath = 'db/base.db';
