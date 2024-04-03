fef7f244cae33422b90b22a8fedc6d61793eafdc
Émile Pharand <emile.pharand@gmail.com>
2024-04-02 21:10:09 -0400

    Rename to UniqueIdeaExpression

--- server/model/ideas/uniqueExpression.ts
+++ server/model/ideas/uniqueExpression.ts
@@ -1,15 +1,15 @@
 import {removeContext} from '../../utils/expressionStringUtils';
 import {type Expression, type ExpressionForAdding} from './expression';
 
-export type UniqueExpression = {
+export type UniqueIdeaExpression = {
 	languageId: number;
 	text: string;
 };
 
-export function getUniqueExpressionFromExpression(e: Expression): UniqueExpression {
+export function fromExpression(e: Expression): UniqueIdeaExpression {
 	return {languageId: e.language.id, text: removeContext(e.text)};
 }
 
-export function getUniqueExpressionFromExpressionForAdding(e: ExpressionForAdding): UniqueExpression {
+export function fromExpressionForAdding(e: ExpressionForAdding): UniqueIdeaExpression {
 	return {languageId: e.languageId, text: removeContext(e.text)};
 }
