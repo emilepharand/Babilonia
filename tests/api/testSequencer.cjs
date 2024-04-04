7d3e7fb51f8986490460a743e975d9fa652c7b64
Émile Pharand <emile.pharand@gmail.com>
2024-04-03 21:54:11 -0400

    Reorder tests

--- tests/api/testSequencer.cjs
+++ tests/api/testSequencer.cjs
@@ -3,11 +3,15 @@ const Sequencer = require('@jest/test-sequencer').default;
 class CustomTestSequencer extends Sequencer {
 	sort(tests) {
 		const smokeTests = tests.find(test => test.path.includes('smoke'));
+		const migrationTests = tests.find(test => test.path.includes('migration'));
 
-		if (smokeTests) {
-			const otherTests = tests.filter(test => test !== smokeTests);
-			return [smokeTests, ...otherTests];
-		}
+		tests = tests.filter(test => test !== smokeTests && test !== migrationTests);
+
+		// Smoke tests should run first
+		tests.unshift(smokeTests);
+
+		// Migration tests should run last
+		tests.push(migrationTests);
 
 		return tests;
 	}
