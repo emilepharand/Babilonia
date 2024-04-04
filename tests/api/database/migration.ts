d50ad2a5e1cdf1394091ab3271a493281442fc1d
Émile Pharand <emile.pharand@gmail.com>
2024-04-03 21:28:00 -0400

    Parameterize migration tests

--- tests/api/database/migration.ts
+++ tests/api/database/migration.ts
@@ -1,9 +1,13 @@
-import {databaseVersionErrorCode, memoryDatabasePath} from '../../../server/const';
+import fs from 'fs';
+import {currentVersion, databaseVersionErrorCode, memoryDatabasePath} from '../../../server/const';
 import * as ApiUtils from '../../utils/api-utils';
 import {oldVersionDatabaseToMigratePath} from '../../utils/const';
+
 beforeEach(async () => {
 	await ApiUtils.changeDatabaseToMemoryAndDeleteEverything();
 });
+
+const previousVersions = ['2.0', '2.1'];
 
 describe('change database', () => {
 	test('change database to a version to be migrated', async () => {
@@ -15,12 +19,21 @@ describe('change database', () => {
 });
 
 describe('migration', () => {
-	test('migrating 2.1 database to 2.2', async () => {
-		const dbToMigratePath = oldVersionDatabaseToMigratePath;
+	test.each(previousVersions)('migrating database version %s to current version', async version => {
+		function getSimpleDatabaseVersionPath(version: string) {
+			return `tests/db/${version}-simple.db`;
+		}
+
+		const dbToMigratePath = getSimpleDatabaseVersionPath(version);
+		expect(fs.existsSync(dbToMigratePath)).toBe(true);
+
+		const currentVersionPath = getSimpleDatabaseVersionPath(currentVersion);
+		await ApiUtils.changeDatabase(currentVersionPath);
+
 		let res = await ApiUtils.changeDatabase(dbToMigratePath);
 		expect(res.status).toEqual(400);
-		expect((await (await res.json() as any)).error).toEqual(databaseVersionErrorCode);
-		expect(await ApiUtils.getDatabasePath()).toEqual(memoryDatabasePath);
+		expect((await res.json() as { error:string }).error).toEqual(databaseVersionErrorCode);
+		expect(await ApiUtils.getDatabasePath()).toEqual(currentVersionPath);
 
 		res = await ApiUtils.migrateDatabase(dbToMigratePath);
 		expect(res.status).toEqual(200);
