f07487b62dd7acf9467e90df2a42b931f5962b80
Émile Pharand <emile.pharand@gmail.com>
2024-04-04 22:16:36 -0400

    Fix prepare.sh

--- tests/bash/prepare.sh
+++ tests/bash/prepare.sh
@@ -3,8 +3,8 @@
 rm -rf dist/tests/db
 mkdir -p tests/coverage/merged dist/db dist/tests/db dist/tests/dir.db
 cp db/base.db dist/db.db
+cp db/base.db dist/tests/db/current.db
 cp tests/db/* dist/tests/db
-cp dist/tests/db/old-version.db dist/tests/db/old-version-to-migrate.db
 
-cp dist/tests/db/2.1-simple.db dist/tests/db/unwriteable.db
+cp dist/tests/db/2.1.db dist/tests/db/unwriteable.db
 chmod 000 dist/tests/db/unwriteable.db
