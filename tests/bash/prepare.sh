a4c2fa588af53644240557a6d248184d6fe67a76
Émile Pharand <emile.pharand@gmail.com>
2024-04-10 21:22:22 -0400

    Use constant for current version in Bash script

--- tests/bash/prepare.sh
+++ tests/bash/prepare.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+CURRENT_VERSION="2.2"
+
 rm -rf dist/tests/db
 mkdir -p tests/coverage/merged dist/db dist/tests/db dist/tests/dir.db
 cp db/base.db dist/db.db
-cp db/base.db dist/tests/db/2.2.db
+cp db/base.db dist/tests/db/$CURRENT_VERSION.db
 cp tests/db/* dist/tests/db
 
-cp dist/tests/db/2.2.db dist/tests/db/unwriteable.db
+cp dist/tests/db/$CURRENT_VERSION.db dist/tests/db/unwriteable.db
 chmod 000 dist/tests/db/unwriteable.db
