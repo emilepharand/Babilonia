eeb7eb31f57777ddab7b50a6261a99253bcce110
Émile Pharand <emile.pharand@gmail.com>
2024-04-10 20:54:05 -0400

    Remove migration from test sequencer

--- tests/api/testSequencer.cjs
+++ tests/api/testSequencer.cjs
@@ -3,15 +3,11 @@ const Sequencer = require('@jest/test-sequencer').default;
 class CustomTestSequencer extends Sequencer {
 	sort(tests) {
 		const smokeTests = tests.find(test => test.path.includes('smoke'));
-		const migrationTests = tests.find(test => test.path.includes('migration'));
 
-		tests = tests.filter(test => test !== smokeTests && test !== migrationTests);
+		tests = tests.filter(test => test !== smokeTests);
 
 		// Smoke tests should run first
 		tests.unshift(smokeTests);
-
-		// Migration tests should run last
-		tests.push(migrationTests);
 
 		return tests;
 	}
