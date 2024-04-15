f35315ce58d9659fe4af90a3c4f96229abcc88dc
Émile Pharand <emile.pharand@gmail.com>
2024-04-14 22:19:59 -0400

    EOF

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,8 +2,8 @@
 
 PACKAGE_VERSION=$(node -p "require('./package.json').version")
 if [[ "$PACKAGE_VERSION" == *-dev ]]; then
-  echo -e "\n\e[1;33m\nWARNING: The version in package.json ends with -dev."
-  echo -e "If this is a release version, remove -dev from the version number.\e[0m\n"
+    echo -e "\n\e[1;33m\nWARNING: The version in package.json ends with -dev."
+    echo -e "If this is a release version, remove -dev from the version number.\e[0m\n"
 fi
 
 rm -rf node_modules release
