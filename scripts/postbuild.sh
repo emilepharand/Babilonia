d2d0f779d37e5cdd14f1db694be738b143445e79
Émile Pharand <emile.pharand@gmail.com>
2024-04-14 22:19:09 -0400

    Create postbuild.sh

---
+++ scripts/postbuild.sh
@@ -0,0 +1,15 @@
+VERSION=$(node -p "require('./package.json').version")
+SQLITE3_VERSION=$(node -p "require('./package.json').dependencies.sqlite3" | sed 's/\\^//g')
+
+cat <<EOF >dist/package.json
+{
+    "name": "babilonia-release",
+    "version": "$VERSION",
+    "dependencies": {
+        "sqlite3": "$SQLITE3_VERSION"
+    },
+    "scripts": {
+        "start": "node index.cjs"
+    }
+}
+EOF
