1a5052aaf90e7c20f9482d37cdf30774c0b082af
Émile Pharand <emile.pharand@gmail.com>
2024-04-14 22:08:06 -0400

    Create release.sh

---
+++ scripts/release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PACKAGE_VERSION=$(node -p "require('./package.json').version")
+if [[ "$PACKAGE_VERSION" == *-dev ]]; then
+  echo -e "\n\e[1;33m\nWARNING: The version in package.json ends with -dev."
+  echo -e "If this is a release version, remove -dev from the version number.\e[0m\n"
+fi
+
+rm -rf node_modules release
+npm i
+npm run build
+mkdir -p release
+cd dist
+zip -r "$VERSION.zip" *
+mv "$VERSION.zip" ../release
