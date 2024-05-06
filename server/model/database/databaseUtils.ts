cf247347e50e91413f7c88756f93a1b788751237
Émile Pharand <emile.pharand@gmail.com>
2024-05-05 21:54:19 -0400

    Create openDatabase

--- server/model/database/databaseUtils.ts
+++ server/model/database/databaseUtils.ts
@@ -1,4 +1,5 @@
-import {type Database} from 'sqlite';
+import {open, type Database} from 'sqlite';
+import sqlite3 from 'sqlite3';
 
 export async function columnExists(db: Database, tableName: string, columnName: string): Promise<boolean> {
 	const query = `
@@ -6,4 +7,13 @@ export async function columnExists(db: Database, tableName: string, columnName: 
         FROM   pragma_table_info('${tableName}')
         WHERE  name = '${columnName}';`;
 	return await db.get(query) !== undefined;
+}
+
+export async function openDatabase(path: string) {
+	const db = await open({
+		filename: path,
+		driver: sqlite3.Database,
+	});
+	await db.exec('PRAGMA foreign_keys = ON');
+	return db;
 }
