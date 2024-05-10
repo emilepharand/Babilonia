420ccb01a5caa9c012855be03bd5a430cd6947c5
Émile Pharand <emile.pharand@gmail.com>
2024-05-09 21:07:20 -0400

    Refactor migration tests

--- tests/migration/migration.ts
+++ tests/migration/migration.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import {Database} from 'sqlite';
 import DatabaseGuidMigrator from '../../server/model/database/databaseGuidMigrator';
 import {clearDatabaseAndCreateSchema} from '../../server/model/database/databaseInitializer';
 import {openDatabase} from '../../server/model/database/databaseUtils';
@@ -13,131 +14,78 @@ describe('migration', () => {
 		const userDbPath = new TestDatabasePath('migration-user.db').getActualPath();
 		const currentDbPath = new TestDatabasePath('migration-current.db').getActualPath();
 
-		let previousDb;
-		try {
-			previousDb = await openDatabase(previousDbPath);
-			await clearDatabaseAndCreateSchema(previousDb);
-
-			const languageManager = new LanguageManager(previousDb);
-			const ideaManager = new IdeaManager(previousDb, languageManager);
-
-			const languages = [];
-
-			const eventType = ['No Change', 'Edited', 'Context Edited', 'Deleted'];
-			for (const t of eventType) {
-				for (const t2 of eventType) {
-					// eslint-disable-next-line no-await-in-loop
-					languages.push(await languageManager.addLanguage(`L - App ${t} - User ${t2}`));
-				}
-			}
-
-			// One idea per language
-			for (const language of languages) {
-				// eslint-disable-next-line no-await-in-loop
-				await ideaManager.addIdea({ee: [{text: 'Placeholder', languageId: language.id}]});
-			}
-
-			// Idea is changed
-			for (const t of ['No Change', 'Deleted']) {
-				for (const t2 of ['No Change', 'Deleted']) {
-					const ee = [];
-					for (const language of languages) {
-						ee.push({text: `I - App ${t} - User ${t2}`, languageId: language.id});
-					}
-					// eslint-disable-next-line no-await-in-loop
-					await ideaManager.addIdea({ee});
-				}
-			}
-
-			// Expression is changed
-			const ee = [];
-			for (const t of eventType) {
-				for (const t2 of eventType) {
-					for (const language of languages) {
-						ee.push({text: `E - App ${t} - User ${t2}`, languageId: language.id});
-					}
-				}
-			}
-
-			await ideaManager.addIdea({ee});
-
-			previousDb.run('UPDATE languages SET guid = (printf(lower(hex(randomblob(4) || randomblob(2) || \'4\' || randomblob(1) || \'a\' || randomblob(5))))) WHERE guid IS NULL');
-			previousDb.run('UPDATE ideas SET guid = (printf(lower(hex(randomblob(4) || randomblob(2) || \'4\' || randomblob(1) || \'a\' || randomblob(5))))) WHERE guid IS NULL');
-			previousDb.run('UPDATE expressions SET guid = (printf(lower(hex(randomblob(4) || randomblob(2) || \'4\' || randomblob(1) || \'a\' || randomblob(5))))) WHERE guid IS NULL');
-		} finally {
-			// Check if the database is still open
-			if (previousDb) {
-				await previousDb.close();
-			}
-		}
+		await fillPreviousVersionDatabase(previousDbPath);
 
 		fs.copyFileSync(previousDbPath, userDbPath);
 		fs.copyFileSync(previousDbPath, currentDbPath);
 
-		let userDb;
-		try {
-			userDb = await openDatabase(userDbPath);
-			userDb.exec(`
-			DELETE FROM expressions
-			WHERE languageId IN (
-				SELECT id
-				FROM languages
-				WHERE name LIKE '%User Deleted%'
-			)
-			OR text LIKE '%User Deleted%';
+		await fillUserDatabase(userDbPath);
+		await fillCurrentVersionDatabase(currentDbPath);
 
-			DELETE FROM languages
-			WHERE name LIKE '%User Deleted%';
+		await migrateDatabase(userDbPath, currentDbPath);
+	}, 60000);
+});
 
-			DELETE FROM ideas
-			WHERE ideas.id IN (
-			SELECT ideas.id FROM ideas
-			LEFT JOIN expressions ON ideas.id = expressions.ideaId
-			WHERE expressions.ideaId IS NULL
-			);
+async function fillPreviousVersionDatabase(previousDbPath: string) {
+	let previousDb;
+	try {
+		previousDb = await openDatabase(previousDbPath);
+		await clearDatabaseAndCreateSchema(previousDb);
 
-			UPDATE expressions
-			SET text = text || ' (User Edited)'
-			WHERE text LIKE '%User Context Edited%';
+		const languageManager = new LanguageManager(previousDb);
+		const ideaManager = new IdeaManager(previousDb, languageManager);
 
-			UPDATE expressions
-			SET text = text || ' - User Edited',
-			guid = NULL
-			WHERE text LIKE '%User Edited%';
-			
-			UPDATE languages
-			SET name = name || ' - User Edited',
-			guid = NULL
-			WHERE name LIKE '%User Edited%';`);
+		const languages = [];
 
-			const languageManager = new LanguageManager(userDb);
-			const ideaManager = new IdeaManager(userDb, languageManager);
+		const modificationType = ['No Change', 'Edited', 'Context Edited', 'Deleted'];
 
-			const userAddedLanguage = await languageManager.addLanguage('User Added');
-
-			await ideaManager.addIdea({ee: [{text: 'I - User Added', languageId: userAddedLanguage.id}]});
-
-			const ideas = await ideaManager.getIdeas();
-			const languages = await languageManager.getLanguages();
-
-			for (const idea of ideas) {
-				const ifa = getIdeaForAddingFromIdea(idea);
-				for (const language of languages) {
-					ifa.ee.push({text: 'E - User Added', languageId: language.id});
-				}
+		for (const appModificationType of modificationType) {
+			for (const userModificationType of modificationType) {
 				// eslint-disable-next-line no-await-in-loop
-				await ideaManager.editIdea(ifa, idea.id);
-			}
-		} finally {
-			if (userDb) {
-				await userDb.close();
+				languages.push(await languageManager.addLanguage(`Language - App ${appModificationType} - User ${userModificationType}`));
 			}
 		}
 
-		let currentDb;
-		try {
-			currentDb = await openDatabase(currentDbPath);
-			currentDb.exec(`
+		for (const language of languages) {
+			// eslint-disable-next-line no-await-in-loop
+			await ideaManager.addIdea({ee: [{text: `Placeholder for language #${language.id}`, languageId: language.id}]});
+		}
+
+		const ideaModificationType = ['No Change', 'Deleted'];
+		for (const appModificationType of ideaModificationType) {
+			for (const userModificationType of ideaModificationType) {
+				const ee = [];
+				for (const language of languages) {
+					ee.push({text: `Idea - App ${appModificationType} - User ${userModificationType}`, languageId: language.id});
+				}
+				// eslint-disable-next-line no-await-in-loop
+				await ideaManager.addIdea({ee});
+			}
+		}
+
+		const ee = [];
+		for (const appModificationType of modificationType) {
+			for (const userModificationType of modificationType) {
+				for (const language of languages) {
+					ee.push({text: `Expression - App ${appModificationType} - User ${userModificationType}`, languageId: language.id});
+				}
+			}
+		}
+		await ideaManager.addIdea({ee});
+
+		await addGuids(previousDb);
+	} finally {
+		if (previousDb) {
+			await previousDb.close();
+		}
+	}
+}
+
+async function fillCurrentVersionDatabase(currentDbPath: string) {
+	let currentDb;
+	try {
+		currentDb = await openDatabase(currentDbPath);
+		currentDb.exec(`
 			DELETE FROM expressions
 			WHERE languageId IN (
 				SELECT id
@@ -168,17 +116,23 @@ describe('migration', () => {
 			SET name = name || ' - App Edited'
 			WHERE name LIKE '%App Edited%';`);
 
-			const languageManager = new LanguageManager(currentDb);
-			const ideaManager = new IdeaManager(currentDb, languageManager);
+		const languageManager = new LanguageManager(currentDb);
+		const ideaManager = new IdeaManager(currentDb, languageManager);
 
-			const appAddedLanguage = await languageManager.addLanguage('App Added');
+		const appAddedLanguage = await languageManager.addLanguage('App Added');
+		const languages = await languageManager.getLanguages();
 
-			await ideaManager.addIdea({ee: [{text: 'I - App Added', languageId: appAddedLanguage.id}]});
+		await ideaManager.addIdea({ee: [{text: 'Idea - App Added', languageId: appAddedLanguage.id}]});
 
-			const ideas = await ideaManager.getIdeas();
-			const languages = await languageManager.getLanguages();
+		for (const language of languages) {
+			// eslint-disable-next-line no-await-in-loop
+			await ideaManager.addIdea({ee: [{text: 'Expression - App Added', languageId: language.id}]});
+		}
 
-			for (const idea of ideas) {
+		const ideas = await ideaManager.getIdeas();
+
+		for (const idea of ideas) {
+			if (!idea.ee.every(e => e.text.includes('App No Change'))) {
 				const ifa = getIdeaForAddingFromIdea(idea);
 				for (const language of languages) {
 					ifa.ee.push({text: 'E - App Added', languageId: language.id});
@@ -186,28 +140,105 @@ describe('migration', () => {
 				// eslint-disable-next-line no-await-in-loop
 				await ideaManager.editIdea(ifa, idea.id);
 			}
-
-			currentDb.exec('UPDATE languages SET guid = (printf(lower(hex(randomblob(4) || randomblob(2) || \'4\' || randomblob(1) || \'a\' || randomblob(5))))) WHERE guid IS NULL');
-			currentDb.exec('UPDATE ideas SET guid = (printf(lower(hex(randomblob(4) || randomblob(2) || \'4\' || randomblob(1) || \'a\' || randomblob(5))))) WHERE guid IS NULL');
-			currentDb.exec('UPDATE expressions SET guid = (printf(lower(hex(randomblob(4) || randomblob(2) || \'4\' || randomblob(1) || \'a\' || randomblob(5))))) WHERE guid IS NULL');
-		} finally {
-			if (currentDb) {
-				await currentDb.close();
-			}
 		}
 
-		try {
-			userDb = await openDatabase(userDbPath);
-			currentDb = await openDatabase(currentDbPath);
-			const dbGuidMigrator = new DatabaseGuidMigrator(userDb, currentDb);
-			await dbGuidMigrator.migrateGuids();
-		} finally {
-			if (userDb) {
-				await userDb.close();
-			}
-			if (currentDb) {
-				await currentDb.close();
+		await addGuids(currentDb);
+	} finally {
+		if (currentDb) {
+			await currentDb.close();
+		}
+	}
+}
+
+async function fillUserDatabase(userDbPath: string) {
+	let userDb;
+	try {
+		userDb = await openDatabase(userDbPath);
+		userDb.exec(`
+			DELETE FROM expressions
+			WHERE languageId IN (
+				SELECT id
+				FROM languages
+				WHERE name LIKE '%User Deleted%'
+			)
+			OR text LIKE '%User Deleted%';
+
+			DELETE FROM languages
+			WHERE name LIKE '%User Deleted%';
+
+			DELETE FROM ideas
+			WHERE ideas.id IN (
+			SELECT ideas.id FROM ideas
+			LEFT JOIN expressions ON ideas.id = expressions.ideaId
+			WHERE expressions.ideaId IS NULL
+			);
+
+			UPDATE expressions
+			SET text = text || ' (User Context Edited)'
+			WHERE text LIKE '%User Context Edited%';
+
+			UPDATE expressions
+			SET text = text || ' - User Edited',
+			guid = NULL
+			WHERE text LIKE '%User Edited%';
+			
+			UPDATE languages
+			SET name = name || ' - User Edited',
+			guid = NULL
+			WHERE name LIKE '%User Edited%';`);
+
+		const languageManager = new LanguageManager(userDb);
+		const ideaManager = new IdeaManager(userDb, languageManager);
+
+		const userAddedLanguage = await languageManager.addLanguage('User Added');
+		const languages = await languageManager.getLanguages();
+
+		await ideaManager.addIdea({ee: [{text: 'Idea - User Added', languageId: userAddedLanguage.id}]});
+
+		for (const language of languages) {
+			// eslint-disable-next-line no-await-in-loop
+			await ideaManager.addIdea({ee: [{text: 'Expression - User Added', languageId: language.id}]});
+		}
+
+		const ideas = await ideaManager.getIdeas();
+		for (const idea of ideas) {
+			if (!idea.ee.every(e => e.text.includes('User No Change'))) {
+				const ifa = getIdeaForAddingFromIdea(idea);
+				for (const language of languages) {
+					ifa.ee.push({text: 'E - User Added', languageId: language.id});
+				}
+				// eslint-disable-next-line no-await-in-loop
+				await ideaManager.editIdea(ifa, idea.id);
 			}
 		}
-	}, 60000);
-});
+	} finally {
+		if (userDb) {
+			await userDb.close();
+		}
+	}
+}
+
+async function migrateDatabase(userDbPath: string, currentDbPath: string) {
+	let userDb;
+	let currentDb;
+	try {
+		userDb = await openDatabase(userDbPath);
+		currentDb = await openDatabase(currentDbPath);
+		const dbGuidMigrator = new DatabaseGuidMigrator(userDb, currentDb);
+		await dbGuidMigrator.migrateGuids();
+	} finally {
+		if (userDb) {
+			await userDb.close();
+		}
+		if (currentDb) {
+			await currentDb.close();
+		}
+	}
+}
+
+async function addGuids(db: Database) {
+	const updateGuid = async (tableName: string) => {
+		await db.run(`UPDATE ${tableName} SET guid = (printf(lower(hex(randomblob(4) || randomblob(2) || '4' || randomblob(1) || 'a' || randomblob(5))))) WHERE guid IS NULL`);
+	};
+	['languages', 'ideas', 'expressions'].forEach(updateGuid);
+}
