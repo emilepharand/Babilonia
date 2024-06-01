c49b30f9def0c7ad11fcd5053408bcd8135a1315
Émile Pharand <emile.pharand@gmail.com>
2024-06-01 14:50:54 -0400

    Refactor databaseGuidMigrator.ts

--- server/model/database/databaseGuidMigrator.ts
+++ server/model/database/databaseGuidMigrator.ts
@@ -10,8 +10,8 @@ export default class DatabaseGuidMigrator {
 	async migrateGuids(): Promise<void> {
 		await this.createColumns();
 		await this.updateGuidsForOldIds();
-		await this.updateGuids();
-		await this.insertGuids();
+		await this.updateContent();
+		await this.insertContent();
 	}
 
 	private async createColumns() {
@@ -42,90 +42,102 @@ export default class DatabaseGuidMigrator {
 		await this._databaseToMigrate.exec(sql);
 	}
 
-	private async updateGuids(): Promise<void> {
+	private async updateContent(): Promise<void> {
 		const sql: string[] = [];
 		const params: unknown[][] = [];
 
-		await this.updateLanguageGuids(sql, params);
-		await this.updateExpressionGuids(sql, params);
-		await this.updateIdeaGuids(sql, params);
+		await this.updateLanguages(sql, params);
+		await this.updateExpressions(sql, params);
+		await this.updateIdeas(sql, params);
 
 		await this.execSqlWithParams(sql, params);
 
 		await this._databaseToMigrate.run('DELETE FROM ideas WHERE id NOT IN (SELECT DISTINCT ideaId FROM expressions)');
 	}
 
-	private async execSqlWithParams(sql: string[], params: unknown[][]) {
-		for (let i = 0; i < sql.length; i++) {
-			// eslint-disable-next-line no-await-in-loop
-			await this._databaseToMigrate.run(sql[i], params[i]);
-		}
-	}
-
-	private async updateIdeaGuids(sql: string[], params: unknown[][]) {
-		const baseIdeas = await this._baseDatabase.all('SELECT id, guid FROM ideas');
-		const ideasToMigrate: Array<{id: number; guid: string}> = await this._databaseToMigrate.all('SELECT id, guid FROM ideas');
-		for (const ideaToMigrate of ideasToMigrate) {
-			if (ideaToMigrate.guid) {
-				if (!baseIdeas.find(baseIdea => baseIdea.guid === ideaToMigrate.guid)) {
-					sql.push('DELETE FROM expressions WHERE ideaId = ?');
-					params.push([ideaToMigrate.id]);
-					sql.push('DELETE FROM ideas WHERE id = ?');
-					params.push([ideaToMigrate.id]);
-				}
-			}
-		}
-	}
-
-	private async updateExpressionGuids(sql: string[], params: unknown[][]) {
-		const baseExpressions: Array<{guid: string; text: string}> = await this._baseDatabase.all('SELECT guid, text FROM expressions');
-		const expressionsToMigrate = await this._databaseToMigrate.all('SELECT id, guid FROM expressions');
-		for (const expressionToMigrate of expressionsToMigrate) {
-			if (expressionToMigrate.guid) {
-				const baseExpression = baseExpressions.find(baseExpression => baseExpression.guid === expressionToMigrate.guid);
-				if (baseExpression) {
-					sql.push('UPDATE expressions SET text = ? WHERE guid = ?');
-					params.push([baseExpression.text, expressionToMigrate.guid]);
-				} else {
-					sql.push('DELETE FROM expressions WHERE id = ?');
-					params.push([expressionToMigrate.id]);
-				}
-			}
-		}
-	}
-
-	private async updateLanguageGuids(sql: string[], params: unknown[][]) {
+	private async updateLanguages(sql: string[], params: unknown[][]) {
 		const baseLanguages: Array<{guid: string; name: string}> = await this._baseDatabase.all('SELECT guid, name FROM languages');
-		const languagesToMigrate = await this._databaseToMigrate.all('SELECT id, guid FROM languages');
-		for (const languageToMigrate of languagesToMigrate) {
-			if (languageToMigrate.guid) {
-				const baseLanguage = baseLanguages.find(baseLanguage => baseLanguage.guid === languageToMigrate.guid);
-				if (baseLanguage) {
-					sql.push('UPDATE languages SET name = ? WHERE guid = ?');
-					params.push([baseLanguage.name, languageToMigrate.guid]);
-				} else {
-					sql.push('DELETE FROM expressions WHERE languageId = ?');
-					params.push([languageToMigrate.id]);
-					sql.push('DELETE FROM languages WHERE id = ?');
-					params.push([languageToMigrate.id]);
-				}
+		const languagesInDb = await this._databaseToMigrate.all('SELECT id, guid FROM languages WHERE guid IS NOT NULL');
+
+		for (const languageInDb of languagesInDb) {
+			const matchingBaseLanguage = baseLanguages.find(baseLanguage => baseLanguage.guid === languageInDb.guid);
+			if (matchingBaseLanguage) {
+				sql.push('UPDATE languages SET name = ? WHERE guid = ?');
+				params.push([matchingBaseLanguage.name, languageInDb.guid]);
+			} else {
+				sql.push('DELETE FROM expressions WHERE languageId = ?');
+				params.push([languageInDb.id]);
+				sql.push('DELETE FROM languages WHERE guid = ?');
+				params.push([languageInDb.guid]);
 			}
 		}
 	}
 
-	private async insertGuids(): Promise<void> {
+	private async updateExpressions(sql: string[], params: unknown[][]) {
+		const commonSelect = 'SELECT guid, text FROM expressions';
+		const baseExpressions: Array<{guid: string; text: string}> = await this._baseDatabase.all(commonSelect);
+		const expressionsInDb = await this._databaseToMigrate.all(`${commonSelect} WHERE guid IS NOT NULL`);
+
+		for (const expressionInDb of expressionsInDb) {
+			const matchingBaseExpression = baseExpressions.find(base => base.guid === expressionInDb.guid);
+			if (matchingBaseExpression) {
+				sql.push('UPDATE expressions SET text = ? WHERE guid = ?');
+				params.push([matchingBaseExpression.text, expressionInDb.guid]);
+			} else {
+				sql.push('DELETE FROM expressions WHERE guid = ?');
+				params.push([expressionInDb.guid]);
+			}
+		}
+	}
+
+	private async updateIdeas(sql: string[], params: unknown[][]) {
+		const commonSelect = 'SELECT id, guid FROM ideas';
+		const baseIdeas = await this._baseDatabase.all(commonSelect);
+		const ideasToMigrate: Array<{id: number; guid: string}> = await this._databaseToMigrate.all(`${commonSelect} WHERE guid IS NOT NULL`);
+		const ideasToDelete = ideasToMigrate.filter(ideaToMigrate => !baseIdeas.some(baseIdea => baseIdea.guid === ideaToMigrate.guid));
+
+		sql.push(...ideasToDelete.map(() => 'DELETE FROM expressions WHERE ideaId = ?'));
+		params.push(...ideasToDelete.map(idea => [idea.id]));
+
+		sql.push(...ideasToDelete.map(() => 'DELETE FROM ideas WHERE id = ?'));
+		params.push(...ideasToDelete.map(idea => [idea.id]));
+	}
+
+	private async insertContent(): Promise<void> {
 		const sql: string[] = [];
 		const params: unknown[][] = [];
 
-		await this.insertLanguageGuids(sql, params);
-		await this.insertIdeaGuids(sql, params);
-		await this.insertExpressionGuids(sql, params);
+		await this.insertBaseLanguages(sql, params);
+		await this.insertBaseIdeas(sql, params);
+		await this.insertBaseExpressions(sql, params);
 
 		await this.execSqlWithParams(sql, params);
 	}
 
-	private async insertExpressionGuids(sql: string[], params: unknown[][]) {
-		const baseExpressions: Array<{guid: string; ideaGuid: string; languageGuid: string; text: string; ordering: number}> = await this._baseDatabase.all(`
+	private async insertBaseLanguages(sql: string[], params: unknown[][]) {
+		const baseLanguages = await this._baseDatabase.all('SELECT name, ordering, guid FROM languages');
+		const languagesInDb: Array<{id: number; guid: string}> = await this._databaseToMigrate.all('SELECT id, guid FROM languages');
+		for (const baseLanguage of baseLanguages) {
+			if (!languagesInDb.find(language => language.guid === baseLanguage.guid)) {
+				sql.push('INSERT INTO languages (name, ordering, guid, isPractice) VALUES (?, ?, ?, 0)');
+				params.push([baseLanguage.name, baseLanguage.ordering, baseLanguage.guid]);
+			}
+		}
+	}
+
+	private async insertBaseIdeas(sql: string[], params: unknown[][]) {
+		const baseIdeas = await this._baseDatabase.all('SELECT guid FROM ideas');
+		const ideasInDb: Array<{guid: string}> = await this._databaseToMigrate.all('SELECT guid FROM ideas');
+		for (const baseIdea of baseIdeas) {
+			if (!ideasInDb.find(idea => idea.guid === baseIdea.guid)) {
+				sql.push('INSERT INTO ideas (guid) VALUES (?)');
+				params.push([baseIdea.guid]);
+			}
+		}
+	}
+
+	private async insertBaseExpressions(sql: string[], params: unknown[][]) {
+		const baseExpressions = await this._baseDatabase.all(`
 			SELECT e.guid, i.guid as ideaGuid, l.guid as languageGuid, text, e.ordering
 			FROM expressions e
 			INNER JOIN ideas i ON i.id = e.ideaId
@@ -133,35 +145,17 @@ export default class DatabaseGuidMigrator {
 		`);
 		const expressionsToMigrate: Array<{guid: string}> = await this._databaseToMigrate.all('SELECT guid FROM expressions');
 		for (const baseExpression of baseExpressions) {
-			const expressionToMigrate = expressionsToMigrate.find(expression => expression.guid === baseExpression.guid);
-			if (!expressionToMigrate) {
+			if (!expressionsToMigrate.find(expression => expression.guid === baseExpression.guid)) {
 				sql.push('INSERT INTO expressions (guid, ideaId, languageId, text, ordering) VALUES (?, (SELECT id FROM ideas WHERE guid = ?), (SELECT id FROM languages WHERE guid = ?), ?, ?)');
 				params.push([baseExpression.guid, baseExpression.ideaGuid, baseExpression.languageGuid, baseExpression.text, baseExpression.ordering]);
 			}
 		}
 	}
 
-	private async insertLanguageGuids(sql: string[], params: unknown[][]) {
-		const baseLanguages: Array<{name: string; ordering: number; guid: string}> = await this._baseDatabase.all('SELECT name, ordering, guid FROM languages');
-		const languagesToMigrate: Array<{id: number; guid: string}> = await this._databaseToMigrate.all('SELECT id, guid FROM languages');
-		for (const baseLanguage of baseLanguages) {
-			const languageToMigrate = languagesToMigrate.find(language => language.guid === baseLanguage.guid);
-			if (!languageToMigrate) {
-				sql.push('INSERT INTO languages (name, ordering, guid, isPractice) VALUES (?, ?, ?, 0)');
-				params.push([baseLanguage.name, baseLanguage.ordering, baseLanguage.guid]);
-			}
-		}
-	}
-
-	private async insertIdeaGuids(sql: string[], params: unknown[][]) {
-		const baseIdeas = await this._baseDatabase.all('SELECT guid FROM ideas');
-		const ideasToMigrate: Array<{guid: string}> = await this._databaseToMigrate.all('SELECT guid FROM ideas');
-		for (const baseIdea of baseIdeas) {
-			const ideaToMigrate = ideasToMigrate.find(idea => idea.guid === baseIdea.guid);
-			if (!ideaToMigrate) {
-				sql.push('INSERT INTO ideas (guid) VALUES (?)');
-				params.push([baseIdea.guid]);
-			}
+	private async execSqlWithParams(sql: string[], params: unknown[][]) {
+		for (let i = 0; i < sql.length; i++) {
+			// eslint-disable-next-line no-await-in-loop
+			await this._databaseToMigrate.run(sql[i], params[i]);
 		}
 	}
 }
