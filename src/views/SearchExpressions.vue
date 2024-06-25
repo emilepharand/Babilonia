a034654b86aac4f1efdc4041950bee7b1772193e
Émile Pharand <emile.pharand@gmail.com>
2024-06-24 20:00:56 -0400

    Make API return expressions

--- src/views/SearchExpressions.vue
+++ src/views/SearchExpressions.vue
@@ -7,44 +7,22 @@
 
 <script lang="ts" setup>
 import DataTable from 'datatables.net-bs5';
-import {type SearchContext} from '../../server/model/search/searchContext';
 import * as Api from '../ts/api';
 
 (async () => {
-	const searchResults = await Api.searchIdeas(createSearchContext());
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
-	const toGive = [] as any;
-	Object.values(searchResults).forEach(idea => {
-		const ideaId = idea.id;
-		for (const e of idea.ee) {
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-			toGive.push({
-				ideaId,
-				language: e.language.name,
-				text: e.text,
-				matched: e.matched,
-				known: e.known,
-			});
-		}
-	});
-	console.log(toGive);
+	const searchResults = await Api.getExpressions();
+	console.log(searchResults);
 
 	// eslint-disable-next-line no-new
 	new DataTable('#table', {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-		data: toGive,
+
+		data: searchResults,
 		columns: [
 			{data: 'ideaId', title: 'Idea ID'},
-			{data: 'language', title: 'Language'},
+			{data: 'languageName', title: 'Language'},
 			{data: 'text', title: 'Text'},
 			{data: 'known', title: 'Known'},
 		],
 	});
 })();
-
-function createSearchContext() {
-	const sc: SearchContext = {};
-	sc.knownExpressions = false;
-	return sc;
-}
 </script>
